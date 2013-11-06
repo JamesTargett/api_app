8c926430012e8999434b16405ba176297730a657
James Targett <someguynamedjamestargett@gmail.com>
2013-11-06 23:45:07 +0000

    second commit

--- jcode.js
+++ jcode.js
@@ -1,8 +1,21 @@
-// jQuery code goes here.
+$.getJSON('https://api.instagram.com/v1/tags/lighthouse/media/recent?access_token=480503655.f59def8.b826b1f518c743bdbcad78929bff525f&callback=?', function(json) {
 
-$(document).ready(function() {
+	var div = $('<div></div>');
+	var post = json['data'][0];
+	console.log(post);
 
-    // Your code here.
+	// get the source of the image so we can set it on the <img> tag later
+	var imageSrc = post.images.standard_resolution.url;
+	
+	// Create <img> tag in memory, with src = whatever instagram gives us
+	var img = $('<img/>').attr('src', imageSrc);
+
+	// append the <img> tag to our DIV
+	$('div#lighthouse').append(img)
 
 
+
+
+	// var img = $('<img />').attr('src', imageSrc);
+	// div.append(img);
 });
