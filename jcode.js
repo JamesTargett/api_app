ffac6f576af983ba78e3b385c285bd406473488b
James Targett <someguynamedjamestargett@gmail.com>
2013-11-09 02:23:50 +0000

    jquery basic functionality working

--- jcode.js
+++ jcode.js
@@ -1,21 +1,36 @@
 $.getJSON('https://api.instagram.com/v1/tags/lighthouse/media/recent?access_token=480503655.f59def8.b826b1f518c743bdbcad78929bff525f&callback=?', function(json) {
 
-	var div = $('<div></div>');
-	var post = json['data'][0];
-	console.log(post);
+	// from each array (post) 
+	$.each(json.data, function(i, post) {
 
-	// get the source of the image so we can set it on the <img> tag later
-	var imageSrc = post.images.standard_resolution.url;
-	
-	// Create <img> tag in memory, with src = whatever instagram gives us
-	var img = $('<img/>').attr('src', imageSrc);
+		// find image source (url)
+		var imageSrc = post.images.standard_resolution.url;
 
-	// append the <img> tag to our DIV
-	$('div#lighthouse').append(img)
+		// create <img> in memory
+		// src = imageSrc from above
+		var img = $('<img/>').attr('src', imageSrc);
+
+		// append <img> to <div>
+		$('div#lighthouse').append(img);
+	});
+
+	// add .active class to first <img> tag added to the div
+	var startingImg = $('img:first').addClass('active');
+
+	var cycleImages = setInterval(function() {
+		// move .active class to next <img> tag
+		var currentImg = $('.active');
+		var nextImg = currentImg.next();
+		// see if nextImg is not there, go to start
+		if (nextImg.length == 0) {
+			nextImg = startingImg;
+		}
+
+		nextImg.addClass('active');
+		currentImg.removeClass('active');
+
+		// if .active is on the last <img> in array, move .active back to the beginning <img> of the array 
+	}, 3000);
 
 
-
-
-	// var img = $('<img />').attr('src', imageSrc);
-	// div.append(img);
 });
