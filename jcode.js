8b32f5994fc33408c439f454b29004e9feb06e89
James Targett <someguynamedjamestargett@gmail.com>
2013-11-09 05:51:29 +0000

    caption feature added --no design yet

--- jcode.js
+++ jcode.js
@@ -5,31 +5,40 @@ $.getJSON('https://api.instagram.com/v1/tags/lighthouse/media/recent?access_toke
 
 		// find image source (url)
 		var imageSrc = post.images.standard_resolution.url;
+		var captionSrc = post.caption.text;
 
 		// create <img> in memory
 		// src = imageSrc from above
 		var img = $('<img/>').attr('src', imageSrc);
+		var caption = $('<p>' + captionSrc + '</p>');
 
 		// append <img> to <div>
-		$('div#lighthouse').append(img);
+		$('div#lighthouseimages').append(img);
+		$('div#lighthousecaptions').append(caption);
 	});
 
 	// add .active class to first <img> tag added to the div
 	var startingImg = $('img:first').addClass('active');
+	var startingCaption = $('p:first').addClass('active');
+
 
 	var cycleImages = setInterval(function() {
 		// move .active class to next <img> tag
 		var currentImg = $('.active');
 		var nextImg = currentImg.next();
+		var currentCaption = $('.active');
+		var nextCaption = currentCaption.next();
+
 		// see if nextImg is not there, go to start
-		if (nextImg.length == 0) {
+		if ((nextImg.length == 0) && (nextCaption.length == 0)) {
 			nextImg = startingImg;
+			nextCaption = startingCaption;
 		}
 
 		nextImg.addClass('active');
+		nextCaption.addClass('active');
 		currentImg.removeClass('active');
-
-		// if .active is on the last <img> in array, move .active back to the beginning <img> of the array 
+		currentCaption.removeClass('active');
 	}, 3000);
 
 
